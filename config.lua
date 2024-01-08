46050ffd051fdbbc40dc9fa7b0e843d6d8a261ae
dexter9006 <52006237+dexter9006@users.noreply.github.com>
2024-01-08 23:09:48 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target interactions (don't change this, go to your server.cfg and add `setr UseTarget true` to use this and just that from true to false or the other way around)
 
-Config.MaxInventoryWeight = 120000                           -- Max weight a player can carry (default 120kg, written in grams)
+Config.MaxInventoryWeight = 100 * 1000                       -- Max weight a player can carry (default 120kg, written in grams)
 Config.MaxInventorySlots = 41                                -- Max inventory slots for a player
 
 Config.KeyBinds = {
@@ -12,17 +12,18 @@ Config.KeyBinds = {
 
 Config.CleanupDropTime = 15 * 60                -- How many seconds it takes for drops to be untouched before being deleted
 Config.MaxDropViewDistance = 12.5               -- The distance in GTA Units that a drop can be seen
-Config.UseItemDrop = false                      -- This will enable item object to spawn on drops instead of markers
+Config.UseItemDrop = true                      -- This will enable item object to spawn on drops instead of markers
 Config.ItemDropObject = `prop_nigel_bag_pickup` -- if Config.UseItemDrop is true, this will be the prop that spawns for the item
 
 Config.VendingObjects = {
     'prop_vend_soda_01',
     'prop_vend_soda_02',
-    'prop_vend_water_01'
+    'prop_vend_water_01',
+    --"prop_vend_coffe_01",
 }
 
 Config.BinObjects = {
-    'prop_bin_05a',
+    --'prop_bin_05a',
 }
 
 Config.CraftingObject = `prop_toolchest_05`
@@ -384,7 +385,9 @@ BackEngineVehicles = {
     [`surfer2`] = true,
     [`gp1`] = true,
     [`autarch`] = true,
-    [`tyrant`] = true
+    [`tyrant`] = true,
+    -- Vitto Vanilla
+    [`turismoo`] = true,
 }
 
 Config.MaximumAmmoValues = {
